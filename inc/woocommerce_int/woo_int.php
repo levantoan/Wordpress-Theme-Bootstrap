b749712be160caa0dcdf667884355878ef7430db
levantoan <levantoan01@gmail.com>
2015-06-26 23:51:31 +0700

    Fix + add update mini cart by ajax when add to cart

--- inc/woocommerce_int/woo_int.php
+++ inc/woocommerce_int/woo_int.php
@@ -24,3 +24,20 @@ function woocommerce_header_add_to_cart_fragment( $fragments ) {
 	$fragments['a.cart-contents'] = ob_get_clean();
 	return $fragments;
 }
+/*Action mini cart*/
+function mini_cart_int(){?>
+	<div class="mini-cart-body">
+	<?php woocommerce_mini_cart();?>
+	</div>
+<?php
+}
+add_action('devvn_mini_cart','mini_cart_int');
+/*Woocommerce update minicart with Ajax*/
+add_filter('add_to_cart_fragments', 'woocommerce_mini_cart_fragment');
+function woocommerce_mini_cart_fragment( $fragments ) {
+	global $woocommerce;
+	ob_start();
+	do_action('devvn_mini_cart');
+	$fragments['.mini-cart-body'] = ob_get_clean();
+	return $fragments;
+}
