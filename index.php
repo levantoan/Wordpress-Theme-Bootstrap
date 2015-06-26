ac3e51c99739cfd60af84a8c8ee13815e65dd74e
levantoan <levantoan01@gmail.com>
2015-06-26 15:27:51 +0700

    Add link Favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="shortcut icon" href="" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <?php wp_head(); ?>
