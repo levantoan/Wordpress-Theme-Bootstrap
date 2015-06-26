84c6110752a71bdfafe926b3d1828804ef183315
levantoan <levantoan01@gmail.com>
2015-06-26 13:17:14 +0700

    Add Logo, Favicon, Copyright Text to Advanced Custom Field

--- inc/advancedCustomField/advancedCustomField.php
+++ inc/advancedCustomField/advancedCustomField.php
@@ -9,3 +9,67 @@ function my_acf_options_page_settings( $settings )
 }
 
 add_filter('acf/options_page/settings', 'my_acf_options_page_settings');
+//Logo, Favicon, Copyright Text
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_general',
+		'title' => 'General',
+		'fields' => array (
+			array (
+				'key' => 'field_5589102012fff',
+				'label' => 'General',
+				'name' => '',
+				'type' => 'tab',
+			),
+			array (
+				'key' => 'field_5589102f13000',
+				'label' => 'Logo',
+				'name' => 'logo',
+				'type' => 'image',
+				'save_format' => 'url',
+				'preview_size' => 'thumbnail',
+				'library' => 'all',
+			),
+			array (
+				'key' => 'field_558cedbdb61d9',
+				'label' => 'Favicon',
+				'name' => 'favicon',
+				'type' => 'image',
+				'save_format' => 'url',
+				'preview_size' => 'thumbnail',
+				'library' => 'all',
+			),
+			array (
+				'key' => 'field_55892dd9b6c66',
+				'label' => 'Copyright text',
+				'name' => 'copyright_text',
+				'type' => 'text',
+				'default_value' => '© 2015 Copyright All Rights Reserved',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'acf-options-general',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
