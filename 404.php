063a4f39f3d97bafc2cbbc01d5b1502ae76c1ef1
levantoan <levantoan01@gmail.com>
2016-02-04 13:40:48 +0700

    no message

--- 404.php
+++ 404.php
@@ -1,9 +1,8 @@
 <?php get_header();?>
 <div class="container">
 	<div class="row">
-		<div class="conl-xs-12 text-center">
+		<div class="col-xs-12 text-center" style="padding: 30px 0; color: red;">
 			<h1>Oh No! That Page Doesn't Exist!</h1>
-			<?php echo get_search_form();?>
 		</div>
 	</div>
 </div>
