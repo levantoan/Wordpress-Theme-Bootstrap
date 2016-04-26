aa86abd124424b7e5b34071ddf888d275511e311
levantoan <levantoan01@gmail.com>
2016-04-26 15:37:12 +0700

    no message

--- functions.php
+++ functions.php
@@ -65,7 +65,7 @@ function theme_slug_widgets_init() {
         'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="title-sidebar">',
-		'after_title'   => '</h3',
+		'after_title'   => '</h3>',
     ));
 }
 //Title
