d17527114cc2d7c72d56566109cdb9f5aeec338e
Lê Văn Toản <levantoan@users.noreply.github.com>
2017-06-01 11:21:34 +0700

    Update woo_int.php

--- inc/woocommerce_int/woo_int.php
+++ inc/woocommerce_int/woo_int.php
@@ -3,6 +3,9 @@
 add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
 	add_theme_support( 'woocommerce' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
 }
 /*woo mini cart*/
 function woo_mini_cart_int(){
