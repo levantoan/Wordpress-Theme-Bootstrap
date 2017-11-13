312c37f9714028e34ac62ea9ab7845415ac9403e
Lê Văn Toản <levantoan01@gmail.com>
2017-11-14 00:05:57 +0700

    add viewport

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no">
 	    <?php wp_head(); ?>
   	</head>
   	<body <?php body_class();?> itemscope itemtype="http://schema.org/WebPage">
